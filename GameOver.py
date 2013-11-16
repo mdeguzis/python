ce700f2e7889438ed2c6fe8795469195593c83b2
ProfessorKaos64 <mdeguzis@gmail.com>
2013-11-15 20:23:26 -0500

    GameOver.py, Code template

---
+++ GameOver.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+"""
+Program Name
+    Game over program, much like hello world
+
+Description 
+    simple hello world type program
+
+Author 
+    ProfessorKaos64
+Date
+    20131115
+
+Revision
+
+"""
+
+print ("Game Over")
+
