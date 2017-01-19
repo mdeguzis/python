b21d3e5d9b87154e8a8867ec2517e3ccab3b920e
professorkaos64 <mdeguzis@gmail.com>
2017-01-18 21:05:30 -0500

    work on tic-tac-toe-game

--- fundamentals/tic-tac-toe.py
+++ fundamentals/tic-tac-toe.py
@@ -3,6 +3,13 @@
 # Demonstratres programmer-created functions
 # Written for python 2
 
+# global constants
+X = "X"
+O = "O"
+EMPTY = " "
+TIE = "TIE"
+NUM_SQUARES = 9
+
 def instructions():
 
 	"""Display game instructions."""
@@ -23,10 +30,37 @@ def instructions():
 	The ultimate battle is about to begin.
 	"""
 
+def ask_yes_no(question):
+	"""Ask a yes or no question"""
+	response = None
+	while response not in ("y", "n")
+		response = raw_input(question).lower()
+	return response
+
+def pieces():
+		"""Determine if player or computer goes first."""
+		go_first = ask_yes_no("Do you require the first move? (y/n): ")
+		if go_first == "y":
+				print "\nThen take the first move. You will need it."
+				human = X
+				computer = O
+			else:
+				print "\nYour bravery will be you undoing... I will go first."
+				computer = X
+				human = O
+			return computer, human
+def new_board():
+	"""Create new game board."""
+	board = []
+	for square in range(NUM_SQUARES):
+		board.append(EMPTY)
+	return board
+
 # main
 print "Here are the instructions to the Tic-Tac-Toe game:"
 instructions()
 
 raw_input("\n\nPress the enter key to exit.")
 
-	
+
+			
