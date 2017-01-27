a83b1d701c02edce868f1a881104675f40d6858a
professorkaos64 <mdeguzis@gmail.com>
2017-01-26 20:35:54 -0500

    basic cpu load monitoring

--- utility/resource_monitor.py
+++ utility/resource_monitor.py
@@ -11,6 +11,9 @@ Once we know when/if that's happening we can tweak from there based on
 what we see on the actual systems during heavy job use.
 '''
 
+
+from multiprocessing import Pool
+from multiprocessing import cpu_count
 import argparse
 import subprocess
 import time
@@ -19,6 +22,8 @@ import socket
 import sys
 import os
 import glob
+import time
+import datetime
 
 # Other vars
 origin_host = socket.gethostname()
@@ -44,17 +49,53 @@ logging.getLogger().addHandler(stdoutLogger)
 logging.info("\n-------------------------\nBEGIN LOG FILE\n-------------------------\n") 
 
 aparser = argparse.ArgumentParser(description="Generate AD Groups and Memberships for Ranger")
+aparser.add_argument('-m', '--minutes', help="runtime in minutes")
+aparser.add_argument('-s', '--seconds', help="runtime in seconds")
 aparser.add_argument('-nl', '--nolog', action='store_true', default=False, help="Do not store the log (useful for testing)")
 args = aparser.parse_args()
 
-def sendEmail(msg):
-		""" Sends an email out based on given information """
-		p = subprocess.Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=subprocess.PIPE)
-		p.communicate(msg.as_string())
+# 
+# Parse args
+#
 
+if args.minutes is not None:
+	total_minutes = int(args.minutes)
+	finish_time = datetime.datetime.now() + datetime.timedelta(minutes=total_minutes)
+
+if args.seconds is not None:
+	total_seconds = int(args.seconds)
+	finish_time = datetime.datetime.now() + datetime.timedelta(seconds=total_seconds)
+
+def sendEmail(msg):
+	""" Sends an email out based on given information """
+
+	p = subprocess.Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=subprocess.PIPE)
+	p.communicate(msg.as_string())
+
+def test_cpu_load(finish_time):
+	"""Tests CPU load from a fixed time frame"""
+
+	# Goal: want to know if five minute load average exceeds 2xCPU (on our nodes greater than 80)
+	# and/or if iowait exceeds 30.
+
+	cpu_limit = float(2.10)
+	print "Running OS load averge until:", finish_time
+	while datetime.datetime.now() < finish_time:
+
+		# reduce stdout logging to INFO when testing is done
+		raw_average = os.getloadavg()
+		load_average = {1: raw_average[0], 5: raw_average[1], 15: raw_average[2]}
+		logging.warning("CPU Load sample: " + str(load_average))
+		if raw_average[0] >= cpu_limit:
+			sys.exit("CPU Limit reached! Limiter: " + str(cpu_limit))
+		# Idle for a bit so we are not flooding the log
+		time.sleep(10)
+
+test_cpu_load(finish_time)
 # End log file
 logging.info("\n-------------------------\nEND LOG FILE\n-------------------------\n") 
 
+'''
 # Rename log file and push to HDFS
 log_filename = "/tmp/resource-monitor-log-" + str(time.strftime("%Y%m%d-%H%M%S")) + ".txt"
 try:
@@ -81,3 +122,4 @@ if args.nolog is not None:
 		logging.error("Could not transfer log file. Perhaps sudo timed out")
 else:
 	logging.info("Log file will not be transferred to HDFS")
+'''
