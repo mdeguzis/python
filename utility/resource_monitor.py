d42fc4b8976383fc98d17a454050988457fbb2f9
professorkaos64 <mdeguzis@gmail.com>
2017-01-26 21:45:56 -0500

    better output for running process

--- utility/resource_monitor.py
+++ utility/resource_monitor.py
@@ -11,9 +11,6 @@ Once we know when/if that's happening we can tweak from there based on
 what we see on the actual systems during heavy job use.
 '''
 
-
-from multiprocessing import Pool
-from multiprocessing import cpu_count
 import argparse
 import subprocess
 import time
@@ -43,12 +40,13 @@ logging.basicConfig(filename=log_filename_tmp, level=logging.DEBUG, filemode='w'
 # Log to file and to stdout
 # For the console logger, only show warnings, error, critical
 stdoutLogger=logging.StreamHandler()
-stdoutLogger.setLevel(logging.WARNING)
+stdoutLogger.setLevel(logging.CRITICAL)
 stdoutLogger.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
 logging.getLogger().addHandler(stdoutLogger)
 logging.info("\n-------------------------\nBEGIN LOG FILE\n-------------------------\n") 
 
 aparser = argparse.ArgumentParser(description="Generate AD Groups and Memberships for Ranger")
+aparser.add_argument('-hr', '--hours', help="runtime in hours")
 aparser.add_argument('-m', '--minutes', help="runtime in minutes")
 aparser.add_argument('-s', '--seconds', help="runtime in seconds")
 aparser.add_argument('-nl', '--nolog', action='store_true', default=False, help="Do not store the log (useful for testing)")
@@ -57,6 +55,18 @@ args = aparser.parse_args()
 # 
 # Parse args
 #
+
+# check for conflicting ergs
+if args.minutes is not None and args.seconds is not None:
+	sys.exit("ERROR: Arguments cannot be specified together.")
+elif args.minutes is not None and args.hours is not None:
+	sys.exit("ERROR: Arguments cannot be specified together.")
+elif args.hours is not None and args.seconds is not None:
+	sys.exit("ERROR: Arguments cannot be specified together.")
+
+if args.hours is not None:
+	total_hours = int(args.hours)
+	finish_time = datetime.datetime.now() + datetime.timedelta(seconds=total_hours)
 
 if args.minutes is not None:
 	total_minutes = int(args.minutes)
@@ -76,18 +86,35 @@ def test_cpu_load(finish_time):
 	"""Tests CPU load from a fixed time frame"""
 
 	# Goal: want to know if five minute load average exceeds 2xCPU (on our nodes greater than 80)
-	# and/or if iowait exceeds 30.
+	# and/or if iowait exceeds 30. Generate CPU load with parallel instances of:
+	# 'dd if=/dev/zero of=/dev/null'
 
-	cpu_limit = float(2.10)
+	cpu_limit_1min = float(10.10)
+	cpu_limit_5min = float(5.10)
+	cpu_limit_15min = float(2.10)
+	cpu_limit_threshold = cpu_limit_1min
+	cpu_limit_warning = cpu_limit_threshold / 2
 	print "Running OS load averge until:", finish_time
+	print "CPU threshold limit:", cpu_limit_threshold
+
 	while datetime.datetime.now() < finish_time:
 
 		# reduce stdout logging to INFO when testing is done
 		raw_average = os.getloadavg()
 		load_average = {1: raw_average[0], 5: raw_average[1], 15: raw_average[2]}
-		logging.warning("CPU Load sample: " + str(load_average))
-		if raw_average[0] >= cpu_limit:
-			sys.exit("CPU Limit reached! Limiter: " + str(cpu_limit))
+		logging.info("CPU Load sample: " + str(load_average))
+
+		if raw_average[0] < cpu_limit_threshold:
+			sys.stdout.write('\r'+ 'CPU limit within threshold. Continuing to monitor...')
+			sys.stdout.flush()
+		elif raw_average[0] >= cpu_limit_warning:
+			# Make sure to make the buffer rewrite at least as long as the last to overwrite it all
+			sys.stdout.write('\r'+ 'WARNING: CPU limit at half capacity                 ')
+			sys.stdout.flush()
+			logging.warning("CPU load at half capacity: " + str(load_average))
+		elif raw_average[0] >= cpu_limit_threshold:
+			sys.exit("CPU limit reached! limiter: " + str(cpu_limit_threshold))
+
 		# Idle for a bit so we are not flooding the log
 		time.sleep(10)
 
