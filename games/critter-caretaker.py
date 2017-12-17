ccd2d16d44ba62d871c1c1db56c6e9a18e4d2576
Michael DeGuzis <mdeguzis@gmail.com>
2017-12-17 17:06:38 -0500

    add stock programs from book

--- games/critter-caretaker.py
+++ games/critter-caretaker.py
@@ -1,21 +1,63 @@
 #!/bin/python2
 #
 # Description: Teaches object-oriented basics, such as
-#			   classes and objects 
+#			   classes, objects, attributes, and more
 
 # Define a class based on the built in type, object
 class Critter(object):
 	"""A virtual pet"""
 
+	# Define a basic total method
+	total = 0
+
+	# Define a static class attribute for tracking critter totals
+	@staticmethod
+	def status():
+		print "\nThe total number of critters is", Critter.total
+
+	# Define a contructor
+	def __init__(self, name):
+		print "A new critter has been born."
+		# Create the attribute "name"
+		self.name = name
+		# Update the critter total for the static class attribute above
+		Critter.total += 1
+
+	# Define an attribute string representation
+	# Normally, printing an object outputs something like:
+	#	 <__main__.Critter object at 0x00A0BA90>
+	def __str__(self):
+		rep = "Critter object\n"
+		rep += "name: " + self.name + "\n"
+		return rep
+
+	def talk(self):
+		print "Hi. I'm", self.name, "\n"
+
 	# Define a method
 	def talk(self):
-		print "Hi. I'm an instqance of class Critter"
+		# Output using the previously defined name attribute
+		print "Hi. I'm " + self.name
 
 # main
 # Instantiate our object
-crit = Critter()
+#crit1 = Critter()
+#crit2 = Critter()
 
-# Call the talk method for the crit object
-crit.talk()
+# This line invokes the talk() method of the Critter object assigned to crit
+#crit1.talk()
+#crit2.talk()
+
+# Output attributes. 
+# In this block of code, a parameter is passed to the Critter class
+crit1 = Critter("Poochie")
+crit1.talk()
+crit2 = Critter("Randolph")
+crit2.talk()
+
+print "\nPrinting crit1:"
+print crit1
+print "Directly accessing crit1.name"
+print crit1.name
 
 raw_input("\nPress the enter key to exit.")
