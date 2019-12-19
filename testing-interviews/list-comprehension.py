f76ff94c3ead9f060c2fbb0fa4436de18bf8702c
Michael DeGuzis <mdeguzis@gmail.com>
2019-12-18 20:35:30 -0500

    commit some new examples

---
+++ testing-interviews/list-comprehension.py
@@ -0,0 +1,17 @@
+print("Normal list\n")
+sentence = "the quick brown fox jumps over the lazy dog"
+words = sentence.split()
+word_lengths = []
+for word in words:
+      if word != "the":
+          word_lengths.append(len(word))
+print(words)
+print(word_lengths)
+
+print("\nList comprehension:\n") 
+
+sentence = "the quick brown fox jumps over the lazy dog"
+words = sentence.split()
+word_lengths = [len(word) for word in words if word != "the"]
+print(words)
+print(word_lengths)
