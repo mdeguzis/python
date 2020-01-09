e7ac2de9deeaa126e031b00c9c4c2237d9d7f1c3
Michael DeGuzis <mdeguzis@gmail.com>
2020-01-08 21:35:53 -0500

    find max pop year

--- testing-interviews/death-year.py
+++ testing-interviews/death-year.py
@@ -20,12 +20,13 @@ max_ddate = max(d[2] for d in data)
 # from the first birth, to latest death,
 # How many of these people where alive in the year
 year_pop = {}
+best_pop = 0
+
 for d in range(min_bdate,max_ddate):
     pop_num = 0
     year_pop[d] = {}
     year_pop[d]['names'] = []
     year_pop[d]['total_pop'] = pop_num
-    year_pop[d]['most_pop'] = False
     for p in data:       
         name = p[0]
         bdate = p[1]
@@ -36,5 +37,14 @@ for d in range(min_bdate,max_ddate):
             year_pop[d]['names'].append(name)
 
 # test date
-print(json.dumps(year_pop, indent =4))
+#print(json.dumps(year_pop, indent =4))
 
+# Get best pop year(s)
+import time
+max_pop_year = max(v['total_pop'] for k,v in year_pop.items())
+print("Max year pop num is %s" % max_pop_year)
+print("Year(s) that have this many people:")
+time.sleep(2)
+for k,v in year_pop.items():
+    if v['total_pop'] == max_pop_year:
+        print(k,v['total_pop'])
