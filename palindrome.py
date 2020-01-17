86883f979a0485b1dc5a9892b6efcd70683e86a9
Michael DeGuzis <mdeguzis@gmail.com>
2020-01-17 13:05:36 -0500

    iterator explanation

--- palindrome.py
+++ palindrome.py
@@ -10,18 +10,11 @@ s1 = s1.replace(" ","").casefold()
 # reverse the string
 rev_str = reversed(s1)
 
-# ??? if I print here they don't compare right?
-# What? haha
+# Only print once here for debugging
+# you can consume an iterator only once
+# If you do, rev_str will be blank later
 #print(list(s1))
 #print(list(rev_str))
-'''
-with the print:
-    $ python mtdeguzis$ python palindrome.py 
-    Enter string: racecar
-    ['r', 'a', 'c', 'e', 'c', 'a', 'r']
-    ['r', 'a', 'c', 'e', 'c', 'a', 'r']
-    The string is not a palindrome.
-'''
 
 # check if the string is equal to its reverse
 if list(s1) == list(rev_str):
